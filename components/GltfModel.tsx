31200aee4b5d460ee20318fa650ef3659a9c25f9
BEW111 <bewilliams111@gmail.com>
2023-01-24 22:07:30 -0500

    react three + spring

--- components/GltfModel.tsx
+++ components/GltfModel.tsx
@@ -1,14 +1,18 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useLoader, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Mesh } from "three";
+import { useSpring, animated } from "@react-spring/three";
 
 const startRot = [0.05, Math.PI * 1.22, 0.02];
 
-const GltfModel = ({ modelPath, scale = 40, position = [0, 0, 0] }) => {
+const GltfModel = ({ modelPath, position = [0, 0, 0] }) => {
   const ref = useRef<Mesh>();
   const gltf = useLoader(GLTFLoader, modelPath);
-  const [hovered, hover] = useState(false);
+
+  const [hovered, setHovered] = useState(false);
+  const { scale } = useSpring({ scale: hovered ? 0.4 : 0.38 });
+  const scaleRef = useRef(scale);
 
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame(({ clock }) => {
@@ -19,14 +23,21 @@ const GltfModel = ({ modelPath, scale = 40, position = [0, 0, 0] }) => {
     ref.current.rotation.z =
       startRot[2] + Math.sin(clock.getElapsedTime()) * 0.1;
   });
+
+  useEffect(() => {
+    document.body.style.cursor = hovered ? "move" : "auto";
+  }, [hovered]);
+
   return (
     <>
-      <primitive
-        scale={0.4}
+      <animated.primitive
+        scale={scaleRef.current.to((x) => [x, x, x])}
         ref={ref}
         object={gltf.scene}
         position={position}
         rotation={startRot}
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
       />
     </>
   );
