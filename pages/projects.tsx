6da7579c6f2449b77daf5eb4e108a7428557df62
BEW111 <bewilliams111@gmail.com>
2023-08-13 21:49:36 -0400

    fixed sizing

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,27 +1,37 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 
-import ChipIcon from "../public/projects/chip_512x512_rounded.png";
 import ChipMockup1 from "../public/projects/chip_mockup_1.png";
 import ChipMockup2 from "../public/projects/chip_mockup_2.png";
 import ChipMockup3 from "../public/projects/chip_mockup_3.png";
 
 import BackgroundChip from "../public/projects/bew_chip_background.png";
-
-const openInNewTab = (url) => {
-  const newWindow = window.open(url, "_blank", "noopener,noreferrer");
-  if (newWindow) newWindow.opener = null;
-};
+import BackgroundMM from "../public/projects/bew_mm_background.png";
+import BackgroundPR from "../public/projects/bew_paperrabbit_background.png";
 
 export default function Projects() {
+  const onWheel = (e) => {
+    if (e.deltaY > 10) {
+      ref.current.scrollBy({
+        top: 0,
+        left: e.deltaY,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const ref = useRef(null);
+
   return (
     <div
       id="projects"
-      className="flex h-screen w-full snap-center flex-row overflow-hidden"
+      className="flex h-full snap-x snap-mandatory flex-row overflow-scroll"
+      ref={ref}
+      onWheel={onWheel}
     >
-      <div className="absolute flex h-full w-full items-center justify-center">
-        <div className="relative my-32 flex w-11/12 flex-col overflow-hidden rounded-3xl border-2 border-blue-500 bg-blue-100 p-6 md:w-2/3">
-          <div className="absolute -bottom-36 -right-36 z-0 opacity-25 md:opacity-50">
+      <div className="pointer-events-none relative flex h-screen w-full shrink-0 snap-center items-center justify-center">
+        <div className="relative my-32 flex h-5/6 w-11/12 flex-col overflow-hidden rounded-3xl border-2 border-blue-500 bg-blue-100 p-6 md:w-2/3">
+          <div className="pointer-events-none absolute -bottom-36 -right-36 z-0 opacity-25 md:opacity-50">
             <Image
               src={BackgroundChip}
               width={500}
@@ -57,25 +67,16 @@ export default function Projects() {
               idea for Chip. After lots of development, pitches, and feedback
               from friends, Chip is now in public beta.
             </p>
-            <div className="invisible flex h-0 md:visible md:h-full">
-              <Image
-                src={ChipMockup1}
-                width={1570 / 12}
-                height={2932 / 12}
-                alt="chip mockup 1"
-              />
-              <Image
-                src={ChipMockup2}
-                width={1570 / 12}
-                height={2932 / 12}
-                alt="chip mockup 2"
-              />
-              <Image
-                src={ChipMockup3}
-                width={1570 / 12}
-                height={2932 / 12}
-                alt="chip mockup 3"
-              />
+            <div className="invisible flex h-0 md:visible md:h-1/4">
+              <div className="aspect-[1570/2932]">
+                <Image src={ChipMockup1} alt="chip mockup 1" />
+              </div>
+              <div className="aspect-[1570/2932]">
+                <Image src={ChipMockup2} alt="chip mockup 2" />
+              </div>
+              <div className="aspect-[1570/2932]">
+                <Image src={ChipMockup3} alt="chip mockup 3" />
+              </div>
             </div>
             <p className="text-md mt-4 font-robotoLight text-lg text-blue-400">
               Built with <span className="text-rose-400">React Native</span> |{" "}
@@ -83,6 +84,86 @@ export default function Projects() {
               <span className="text-rose-400">Vision Camera</span> |{" "}
               <span className="text-rose-400">Redux</span> |{" "}
               <span className="text-rose-400">Notifee</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="pointer-events-none relative flex h-screen w-full shrink-0 snap-center items-center justify-center">
+        <div className="relative my-32 flex h-5/6 w-11/12 flex-col overflow-hidden rounded-3xl border-2 border-blue-500 bg-blue-100 p-6 md:w-2/3">
+          <div className="pointer-events-none absolute -bottom-16 -right-16 z-0 opacity-25 md:opacity-50">
+            <Image
+              src={BackgroundPR}
+              width={400}
+              height={400}
+              alt="background image for paperrabbit"
+            />
+          </div>
+          <div className="z-1 relative">
+            <div className="mb-4 flex items-center justify-between gap-4">
+              <div>
+                <p className="mb-2 font-robotoMedium text-3xl text-blue-500 md:text-4xl lg:text-6xl">
+                  PaperRabbit
+                </p>
+                <p className="font-robotoLight text-lg text-blue-500 md:text-2xl lg:text-4xl">
+                  Explore research papers using NLP
+                </p>
+              </div>
+            </div>
+            <p className="mb-4 font-robotoRegular text-sm text-blue-400 md:text-lg">
+              <a
+                className="pointer-events-auto font-robotoMedium text-blue-600 hover:text-rose-500"
+                href="https://www.paperrabbit.xyz"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Check out the website here.
+              </a>{" "}
+              At UC Berkeley's 2023 LLM-themed hackathon, my friend Aagam and I
+              built a website called PaperRabbit to help you organize your
+              research paper reading lists.
+            </p>
+            <p className="text-md mt-4 font-robotoLight text-lg text-blue-400">
+              Built with <span className="text-rose-400">Flask</span> |{" "}
+              <span className="text-rose-400">React</span> |{" "}
+              <span className="text-rose-400">Various NLP tools</span> |{" "}
+              <span className="text-rose-400">OpenAI API</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="pointer-events-none relative flex h-screen w-full shrink-0 snap-center items-center justify-center">
+        <div className="relative my-32 flex h-5/6 w-11/12 flex-col overflow-hidden rounded-3xl border-2 border-blue-500 bg-blue-100 p-6 md:w-2/3">
+          <div className="pointer-events-none absolute -bottom-16 -right-16 z-0 opacity-25 md:opacity-50">
+            <Image
+              src={BackgroundMM}
+              width={400}
+              height={400}
+              alt="background image for milestone monitor"
+            />
+          </div>
+          <div className="z-1 relative">
+            <div className="mb-4 flex items-center justify-between gap-4">
+              <div>
+                <p className="mb-2 font-robotoMedium text-3xl text-blue-500 md:text-4xl lg:text-6xl">
+                  Milestone Monitor
+                </p>
+                <p className="font-robotoLight text-lg text-blue-500 md:text-2xl lg:text-4xl">
+                  Personal goal accountability chatbot
+                </p>
+              </div>
+            </div>
+            <p className="mb-4 font-robotoRegular text-sm text-blue-400 md:text-lg">
+              To learn more about vector databases and conversational AI, my
+              friend Shiva and I built a textable (via SMS) chatbot that helps
+              you come up with structured goals, saves all your data, and sends
+              you customizable reminders.
+            </p>
+            <p className="text-md mt-4 font-robotoLight text-lg text-blue-400">
+              Built with <span className="text-rose-400">LangChain</span> |{" "}
+              <span className="text-rose-400">Pinecone</span> |{" "}
+              <span className="text-rose-400">Cohere</span> |{" "}
+              <span className="text-rose-400">Django</span> |{" "}
+              <span className="text-rose-400">Celery</span>
             </p>
           </div>
         </div>
